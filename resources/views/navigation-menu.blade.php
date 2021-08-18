6593d27031bf7a89a73f8b1c6fe183d9c40f9ebc
cpnx95s <cpnx95s@gmail.com>
2021-08-18 14:18:45 +0700

    เปลี่ยนชื่อโปรเจ็ค กับ ชื่อเมนู

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,7 +12,7 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
+                    {{ __('Home') }}
                 </x-jet-nav-link>
             </ul>
 
