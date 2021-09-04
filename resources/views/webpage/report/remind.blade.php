7383b10578600fd9709cd748f4eb9e5e216c0776
jamepot <84700038+jamepot@users.noreply.github.com>
2021-09-04 14:03:40 +0700

    เเก้หน้า

--- resources/views/webpage/report/remind.blade.php
+++ resources/views/webpage/report/remind.blade.php
@@ -4,5 +4,327 @@
             {{ __('report') }}
         </h2>
     </x-slot>
-    หน้า remind.blade.php
+    <!-- หน้า conclude.blade.php -->
+    <div class="container-fluid">
+        <div class="container mt-3">
+            <pre></pre>
+            <pre></pre>
+            <pre></pre>
+            <pre></pre>
+            <form action="/report">
+                <div class="shadow p-1 sm-1 bg-white">
+                    <div class="row">
+                        <div class="col-sm-1 p-4">
+                            <div class="p-2">
+                                <h6> วันที่:</h6>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 p-4">
+                            <div class="input-group sm-1">
+                                <input type="date" class="form-control" placeholder="Search">
+                                <button type="button" class="btn btn-primary">Option</button>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 p-4">
+
+                            <div class="input-group sm-1">
+                                <div class="col-sm-2 p-2">
+                                    <h6>กลุ่มรถ :</h6>
+                                </div>
+                                <select name="cars" class="custom-select sm-1">
+                                    <option selected>ทั้งหมด</option>
+                                    <option value="KDR">KDR</option>
+                                </select>
+                                <div class="col-sm-2">
+                                    <button type="button" class="btn btn-success">ยืนยัน</button>
+            </form>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <br>
+    <div class="shadow p-1 sm-1 bg-white">
+        <div class="shadow-none p-1 mb-1 bg-light">
+            <div class="custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="selectAll" name="example">
+                <label class="custom-control-label" for="selectAll">เลือกทั้งหมด</label>
+            </div>
+        </div>
+        <div class="shadow p-1 sm-1 bg-white">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input id="your_checkbox_class" type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">2ฒศ-7715
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-check-inline">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" value="">1ฒศ-6515
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-app-layout>
+<div class="container-fluid">
+    <br>
+    <br>
+    <table class="table">
+        <thead class="thead-light">
+            <tr>
+                <th>ลำดับ</th>
+                <th>ทะเบียน</th>
+                <th>พนักงานขับรถ</th>
+                <th>เวลาเริ่มงาน</th>
+                <th>สถานที่ต้นทาง</th>
+                <th>เวลาจบงาน</th>
+                <th>สถานที่ปลายทาง</th>
+                <th>ระยะทาง</th>
+                <th>รวมเวลาทำงาน</th>
+                <th>รวมเวลารถวิ่ง</th>
+                <th>รวมเวลาจอดไม่ดับเครื่อง</th>
+                <th>รวมเวลารถเร็วเกินกำหนด</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>2ฒศ-7715</td>
+                <td>-</td>
+                <td>2021-09-13 00.06.26</td>
+                <td>KDR service gas station</td>
+                <td>2021-09-13 00.11.51</td>
+                <td>คลังลาซาล</td>
+                <td>0.37</td>
+                <td>0 ชม. 5 นาที 25 วินาที</td>
+                <td>0 ชม. 2 นาที 9 วินาที</td>
+                <td>0 ชม. 3 นาที 16 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>2ฒศ-7715</td>
+                <td>-</td>
+                <td>2021-09-13 00.01.46</td>
+                <td>คลังลาซาล</td>
+                <td>2021-09-13 00.12.21</td>
+                <td>คลังลาซาล</td>
+                <td>0.37</td>
+                <td>0 ชม. 10 นาที 25 วินาที</td>
+                <td>0 ชม. 1 นาที 49 วินาที</td>
+                <td>0 ชม. 3 นาที 16 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>2ฒศ-7715</td>
+                <td>-</td>
+                <td>2021-09-13 00.01.46</td>
+                <td>คลังลาซาล</td>
+                <td>2021-09-13 00.12.21</td>
+                <td>คลังลาซาล</td>
+                <td>0.37</td>
+                <td>0 ชม. 10 นาที 25 วินาที</td>
+                <td>0 ชม. 1 นาที 49 วินาที</td>
+                <td>0 ชม. 3 นาที 16 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>2ฒศ-7715</td>
+                <td>-</td>
+                <td>2021-09-13 00.16.26</td>
+                <td>คลังลาซาล</td>
+                <td>2021-09-13 00.19.51</td>
+                <td>คลังลาซาล</td>
+                <td>0.35</td>
+                <td>0 ชม. 2 นาที 15 วินาที</td>
+                <td>0 ชม. 1 นาที 19 วินาที</td>
+                <td>0 ชม. 1 นาที 6 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>รวม</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>20.24</td>
+                <td>1 ชม. 2 นาที 15 วินาที</td>
+                <td>0 ชม. 22 นาที 19 วินาที</td>
+                <td>0 ชม. 46 นาที 6 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1ฒศ-6515</td>
+                <td>-</td>
+                <td>2021-09-13 00.06.26</td>
+                <td>KDR service gas station</td>
+                <td>2021-09-13 00.11.51</td>
+                <td>คลังลาซาล</td>
+                <td>0.37</td>
+                <td>0 ชม. 5 นาที 25 วินาที</td>
+                <td>0 ชม. 2 นาที 9 วินาที</td>
+                <td>0 ชม. 3 นาที 16 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>1ฒศ-6515</td>
+                <td>-</td>
+                <td>2021-09-13 00.01.46</td>
+                <td>คลังลาซาล</td>
+                <td>2021-09-13 00.12.21</td>
+                <td>คลังลาซาล</td>
+                <td>0.37</td>
+                <td>0 ชม. 10 นาที 25 วินาที</td>
+                <td>0 ชม. 1 นาที 49 วินาที</td>
+                <td>0 ชม. 3 นาที 16 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>1ฒศ-6515</td>
+                <td>-</td>
+                <td>2021-09-13 00.01.46</td>
+                <td>คลังลาซาล</td>
+                <td>2021-09-13 00.12.21</td>
+                <td>คลังลาซาล</td>
+                <td>0.37</td>
+                <td>0 ชม. 10 นาที 25 วินาที</td>
+                <td>0 ชม. 1 นาที 49 วินาที</td>
+                <td>0 ชม. 3 นาที 16 วินาที</td>
+                <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+
+            <td></td>
+            <td>รวม</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>20.24</td>
+            <td>1 ชม. 2 นาที 15 วินาที</td>
+            <td>0 ชม. 22 นาที 19 วินาที</td>
+            <td>0 ชม. 46 นาที 6 วินาที</td>
+            <td>0 ชม. 0 นาที 0 วินาที</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
