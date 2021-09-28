bfb553088cf2935ec1115ec54e583d590563550c
อรรถพล มงคล <69508240+bankhk10@users.noreply.github.com>
2021-09-28 11:09:20 +0700

    ลบ home

--- resources/views/dashboard/index.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold h4 text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-    หน้า index.blade.php
-</x-app-layout>
